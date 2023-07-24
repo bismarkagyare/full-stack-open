9ae0379f93eafa8dcd16ecd073fe03e3dec99b66
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-24 12:04:39 +0000

    Implement the button component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,25 @@
-//import { useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 
-const App = () => {};
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={handleClick} />
+      <Button text="neutral" />
+      <Button text="bad" />
+      <h1>statistics</h1>
+    </>
+  );
+};
 
 export default App;

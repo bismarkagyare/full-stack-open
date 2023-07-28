436db1aa047abf98446e3e4a14ae89c48d0b973d
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-28 10:37:28 +0000

    Finish ex_2.6 : Add names to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,19 +5,34 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const addName = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  const handleNameInput = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>;
+      })}
     </div>
   );
 };

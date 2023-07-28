7fcb35d24c683e8d752622087fec65f68a461bbb
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-28 12:29:29 +0000

    Finish ex_2.9: Implement search functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '983-3459083' },
+    { name: 'Arto Hellas', number: '983-3459083', id: 1 },
+    { name: 'Kawtar Barouti', number: '337-2345876', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filterValue, setFilterValue] = useState('');
 
   const addName = (e) => {
     e.preventDefault();
@@ -16,6 +20,7 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
+        id: persons.length + 1,
       };
       setPersons(persons.concat(personObject));
       setNewName('');
@@ -31,9 +36,18 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterInput = (e) => {
+    setFilterValue(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label htmlFor="filter">Filter shown with</label>
+        <input id="filter" value={filterValue} onChange={handleFilterInput} />
+      </div>
+      <h2>Add a new item</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameInput} />
@@ -46,13 +60,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
-        return (
-          <p key={person.name}>
-            {person.name} {person.number}
-          </p>
-        );
-      })}
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filterValue.toLowerCase())
+        )
+        .map((person) => {
+          return (
+            <p key={person.id}>
+              {person.name} {person.number}
+            </p>
+          );
+        })}
     </div>
   );
 };

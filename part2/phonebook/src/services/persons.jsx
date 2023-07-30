5b602a949b8af5097fc39ff1df9c8f2714f151c7
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-30 12:40:34 +0000

    Create update function using put method on server

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -12,9 +12,14 @@ const create = (personObject) => {
   return request.then((response) => response.data);
 };
 
+const update = (id, newPerson) => {
+  const request = axios.put(`${baseUrl}/${id}`, newPerson);
+  return request.then((response) => response.data);
+};
+
 const deletePerson = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
   return request.then((response) => {});
 };
 
-export default { getAll, create, deletePerson };
+export default { getAll, create, update, deletePerson };

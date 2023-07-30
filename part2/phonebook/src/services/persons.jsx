d6867a1cd5ff5f70fdfd281e4186c81e629d94da
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-30 22:11:55 +0000

    Return data from response

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -19,7 +19,7 @@ const update = (id, newPerson) => {
 
 const deletePerson = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => {});
+  return request.then((response) => response.data);
 };
 
 export default { getAll, create, update, deletePerson };

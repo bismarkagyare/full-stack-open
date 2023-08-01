11a4abc23b4b577b935cb4e2ff75d51eaa21f2d3
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-01 18:38:07 +0000

    Finish ex_2.19: Add show button next to filtered countries

--- part2/countries/src/components/countryList.jsx
+++ part2/countries/src/components/countryList.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
+import CountryDetails from './countryDetails';
+import { useEffect } from 'react';
+
 const CountryList = ({ countries, searchQuery, filteredCountries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    // Reset selectedCountry when the searchQuery becomes empty
+    if (searchQuery === '') {
+      setSelectedCountry(null);
+    }
+  }, [searchQuery]);
+
+  const handleShowButton = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
-    <ul>
-      {searchQuery === ''
-        ? countries.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))
-        : filteredCountries.length <= 10 &&
-          filteredCountries.map((country) => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))}
-    </ul>
+    <div>
+      <ul>
+        {searchQuery === ''
+          ? countries.map((country) => (
+              <li key={country.cca3}>{country.name.common}</li>
+            ))
+          : filteredCountries.length <= 10 &&
+            filteredCountries.map((country) => (
+              <div key={country.cca3}>
+                <li style={{ display: 'inline-block', marginRight: '10px' }}>
+                  {country.name.common}
+                </li>
+                <button onClick={() => handleShowButton(country)}>show</button>
+              </div>
+            ))}
+      </ul>
+      {selectedCountry && <CountryDetails country={selectedCountry} />}
+    </div>
   );
 };
 

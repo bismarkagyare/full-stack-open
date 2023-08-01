5a05cdfe3a8d9479f266a3e90b4ce59d0afd4853
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-01 13:00:21 +0000

    Create component for country basic info

---
+++ part2/countries/src/components/countryDetails.jsx
@@ -0,0 +1,20 @@
+const CountryDetails = ({ country }) => {
+  const { name, capital, area, languages, flag } = country;
+
+  return (
+    <div>
+      <h1>{name.common}</h1>
+      <p>capital: {capital}</p>
+      <p>area: {area}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.entries(languages).map(([key, value]) => (
+          <li key={key}>{value}</li>
+        ))}
+      </ul>
+      <p>flag: {flag}</p>
+    </div>
+  );
+};
+
+export default CountryDetails;

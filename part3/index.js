ed27eb5bbd5396e0fd908ce93a31a0c145e576f0
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-03 22:34:12 +0000

    Fix typo

--- part3/index.js
+++ part3/index.js
@@ -35,7 +35,7 @@ let persons = [
 ];
 
 app.get('/', (req, res) => {
-  res.send('<h1>hi there, im learing express</h1>');
+  res.send('<h1>hi there, im learning express</h1>');
 });
 
 app.get('/api/persons', (req, res) => {

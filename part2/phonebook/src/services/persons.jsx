03fb048dec1b23d1adc1d55192af207c934b6108
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-05 11:20:07 +0000

    Use relative url path

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);

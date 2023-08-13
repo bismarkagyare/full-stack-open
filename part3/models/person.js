1396ca45f89a47f303f0e4192861c6413acd9408
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-13 19:55:57 +0000

    Finished ex_3.13

---
+++ part3/models/person.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+console.log('Connecting to ', url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.log('Error connecting to database', error.message);
+  });
+
+const personSchema = new mongoose.Schema(
+  {
+    name: String,
+    number: String,
+  },
+  {
+    collection: 'people',
+  }
+);
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);

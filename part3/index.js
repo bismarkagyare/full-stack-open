b8ac4dff073899fbca07e56d857716d3d222be05
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-14 16:29:13 +0000

    Implement validation in middleware

--- part3/index.js
+++ part3/index.js
@@ -9,6 +9,8 @@ const errorHandler = (error, req, res, next) => {
   console.log(error.message);
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);

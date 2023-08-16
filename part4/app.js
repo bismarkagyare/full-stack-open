a2a2d2770afe09a9c5138f8feafd5fbc78ca6113
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-16 10:29:25 +0000

    Finish ex_4.2: Refactor code

---
+++ part4/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogRouter = require('./controllers/route');
+const logger = require('./utils/logger');
+
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/blogs', blogRouter);
+
+module.exports = app;
--- part4/index.js
+++ part4/index.js
@@ -1,29 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Blog = require('./models/blog');
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+app.listen(config.PORT, '0.0.0.0', () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-  blog.save().then((savedBlog) => {
-    response.status(201).json(savedBlog);
-  });
-});
-
-const PORT = process.env.PORT || 3003;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);

6dc15e7ace4cc9d3467dab5909090c2d01be0219
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-18 14:53:19 +0000

    Finish ex_4.13: Update the routes

--- part4/controllers/route.js
+++ part4/controllers/route.js
@@ -12,6 +12,15 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(204);
+  }
+});
+
 // blogRouter.post('/', (request, response) => {
 //   const blog = new Blog(request.body);
 //   blog.save().then((savedBlog) => {
@@ -23,6 +32,11 @@ blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog);
+});
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogRouter;

72e1e364302015e6feea4a193758640b5c73b6cd
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-18 15:05:26 +0000

    Finish ex_4.14: Implement put handler function

--- part4/controllers/route.js
+++ part4/controllers/route.js
@@ -39,4 +39,19 @@ blogRouter.delete('/:id', async (request, response) => {
   response.status(204).end();
 });
 
+blogRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body;
+  const blog = {
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+
 module.exports = blogRouter;

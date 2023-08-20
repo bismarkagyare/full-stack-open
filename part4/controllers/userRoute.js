c70068d1622d4949a68292177652f002019670c7
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-20 12:53:19 +0000

    Finished ex_4.16: Add restrictions to creating users

--- part4/controllers/userRoute.js
+++ part4/controllers/userRoute.js
@@ -9,12 +9,21 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
-  const saltRounds = 10;
 
+  if (!username || !password) {
+    response.status(400).json({ error: 'both name and password is required' });
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    response
+      .status(400)
+      .json({ error: 'both should be at least 3 characters' });
+  }
+
+  const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({ username, name, passwordHash });
-
   const savedUser = user.save();
   response.status(201).json(savedUser);
 });

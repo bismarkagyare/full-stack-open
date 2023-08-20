15e440ac03dca4378fccf707883579cc370988ec
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-20 18:00:21 +0000

    Finished ex_4.19

--- part4/controllers/route.js
+++ part4/controllers/route.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -16,34 +17,33 @@ blogRouter.get('/:id', async (request, response) => {
   }
 });
 
-// blogRouter.post('/', (request, response) => {
-//   const blog = new Blog(request.body);
-//   blog.save().then((savedBlog) => {
-//     response.status(201).json(savedBlog);
-//   });
-// });
-
 blogRouter.post('/', async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+  const token = body.token;
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user.id,
-  });
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    }
 
-  const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
-  response.status(201).json(savedBlog);
-});
+    const user = await User.findById(decodedToken.id);
 
-blogRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id);
-  response.status(204).end();
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user.id,
+    });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+    response.status(201).json(savedBlog);
+  } catch (error) {
+    response.status(401).json({ error: 'invalid token' });
+  }
 });
 
 blogRouter.put('/:id', async (request, response) => {

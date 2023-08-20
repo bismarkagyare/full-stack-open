7a2f5394f8d8113410882128fcb0e1d5d5083a4a
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-20 13:38:43 +0000

    Finish ex_4.17: Populate user details in the request

--- part4/controllers/route.js
+++ part4/controllers/route.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -23,8 +24,20 @@ blogRouter.get('/:id', async (request, response) => {
 // });
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
+
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 

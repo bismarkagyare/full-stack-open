0acd24d183dfa5d0fcadccad19e00edb8c67f843
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-20 10:44:49 +0000

    Create middleware functions for bloglist

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/route');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 mongoose.set('strictQuery', false);
 
@@ -22,6 +23,11 @@ mongoose
 
 app.use(express.json());
 app.use(cors());
+app.use(middleware.requestLogger);
+
 app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;

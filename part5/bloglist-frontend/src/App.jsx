75efd0ada5db0d8e4a361c5107b1480b71bbb38e
Bismark Agyare <bismarkagyare54@gmail.com>
2023-08-22 17:54:44 +0000

    Initialise components

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Blog from '../components/Blog';
+import blogService from '../services/blogs';
+
 const App = () => {
-  return <h1>hello there</h1>;
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    blogService.getAll().then((initialBlogs) => setBlogs(initialBlogs));
+  }, []);
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
 };
 
 export default App;

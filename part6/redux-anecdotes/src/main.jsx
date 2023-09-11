1f7b827f3d7d5f9e8a26297b8330745fa26ca4c8
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-11 16:47:53 +0000

    Setup reducer function and redux hooks

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/anecdoteReducer.js';
 import App from './App.jsx';
 
+const store = createStore(reducer);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );

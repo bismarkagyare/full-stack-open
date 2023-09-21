9e95a8bbbb6bfffdfe383e0e07107fd732ccf1a9
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-21 21:20:41 +0000

    Update filter reducer using createSlice

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = '';
 
-const filterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter: (state, action) => {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  };
-};
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
 
-export default filterReducer;
+// const initialState = '';
+
+// const filterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   };
+// };
+
+// export default filterReducer;

241b29df4d236a9542e4382709b166ece1727551
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-24 21:37:27 +0000

    Fetch and initialize anecdotes using useEffect

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import NewAnecdote from './components/AnecdoteForm';
 import Anecdotes from './components/AnecdotesList';
 import Filter from './components/Filter';
+import anecdoteService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+  //prettier-ignore
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>

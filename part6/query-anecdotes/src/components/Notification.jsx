cc9ec38d77e5cb25bed6ebd26ac706e03fadde20
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-25 18:45:35 +0000

    Setup notification component

---
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,12 @@
+const Notification = () => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return <div style={style}>notification goes here</div>;
+};
+
+export default Notification;
